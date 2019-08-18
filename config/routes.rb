8e1d1d08f3546d8742c6cea04bb1e862ce4bbd80
Mathieu Robin <mathieu.robin1@gmail.com>
2019-08-18 15:50:08 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'new', to:'games#new'
-  get 'score', to: 'games#score'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'new',    to: 'games#new'
+  post 'score', to: 'games#score'
 end
-
